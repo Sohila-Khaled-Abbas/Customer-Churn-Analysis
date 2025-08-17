a935a139814185ac2a5ab8bd593711b34299fade
Sohila Khaled <agro.sohila.k.abbas@gmail.com>
2025-08-17 20:59:39 +0200

    Update 02_star_schema.sql

--- sql/02_star_schema.sql
+++ sql/02_star_schema.sql
@@ -1,40 +1,34 @@
 -- Drop existing star schema
-DROP TABLE IF EXISTS dim_customer, dim_location, fact_customer_activity CASCADE;
+DROP TABLE IF EXISTS telecom.dim_customer, telecom.dim_location, telecom.fact_customer_activity CASCADE;
 
 -- Dimension Tables
-CREATE TABLE dim_location (
+CREATE TABLE telecom.dim_location (
     location_id SERIAL PRIMARY KEY,
     zip_code VARCHAR(10),
-    city VARCHAR(100),
-    state VARCHAR(50),
-    region VARCHAR(50)
-);
+	population INT);
 
-INSERT INTO dim_location (zip_code, city, state, region)
-SELECT DISTINCT zip_code, city, state, region
-FROM stg_dim_zip_code;
+INSERT INTO telecom.dim_location (zip_code)
+SELECT DISTINCT zip_code
+FROM telecom.stg_zip_code;
 
-CREATE TABLE dim_customer (
-    customer_id INT PRIMARY KEY,
-    name VARCHAR(100),
+CREATE TABLE telecom.dim_customer (
+    customer_id VARCHAR(100),
     age INT,
     gender VARCHAR(10),
-    location_id INT REFERENCES dim_location(location_id),
-    join_date DATE
+    location_id INT REFERENCES telecom.dim_location(location_id)
 );
 
-INSERT INTO dim_customer
-SELECT f.customer_id, f.name, f.age, f.gender, l.location_id, f.join_date
-FROM stg_fact_customers f
-LEFT JOIN dim_location l ON f.zip_code = l.zip_code;
+INSERT INTO telecom.dim_customer
+SELECT f.customer_id, f.age, f.gender, l.location_id
+FROM telecom.stg_fact_customers f
+LEFT JOIN telecom.dim_location l ON f.zip_code = l.zip_code;
 
 -- Fact Table
-CREATE TABLE fact_customer_activity (
-    customer_id INT REFERENCES dim_customer(customer_id),
-    churn_flag BOOLEAN,
-    PRIMARY KEY (customer_id)
+CREATE TABLE telecom.fact_customer_activity (
+    customer_id VARCHAR(100),
+    churn_flag TEXT
 );
 
-INSERT INTO fact_customer_activity
+INSERT INTO telecom.fact_customer_activity
 SELECT customer_id, churn_flag
-FROM stg_fact_customers;
+FROM telecom.stg_fact_customers;
